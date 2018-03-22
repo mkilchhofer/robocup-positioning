20eddfca20da024137b7ebb1c3a1f193779e10e8
Marco Kilchhofer <marco@kilchhofer.info>
2018-03-22 19:54:24 +0100

    Fix path in helper scripts

--- scripts/start-consoleui.sh
+++ scripts/start-consoleui.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar lidar-consoleui-service/target/lidar-consoleui-service-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar ../lidar-consoleui-service/target/lidar-consoleui-service-1.0-SNAPSHOT-jar-with-dependencies.jar
