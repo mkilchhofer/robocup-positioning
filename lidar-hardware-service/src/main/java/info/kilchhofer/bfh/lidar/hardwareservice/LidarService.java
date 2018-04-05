38c10718455b28f6c9835bf4a5b27aa771a53c13
Marco Kilchhofer <marco@kilchhofer.info>
2018-04-05 05:07:04 +0200

    Support multiple sensors in hardware adapter service

--- lidar-hardware-service/src/main/java/info/kilchhofer/bfh/lidar/hardwareservice/LidarService.java
+++ lidar-hardware-service/src/main/java/info/kilchhofer/bfh/lidar/hardwareservice/LidarService.java
@@ -5,12 +5,16 @@ import com.google.common.primitives.Ints;
 import laser.scanner.*;
 import laser.scanner.tim55x.TiM55x;
 import laser.scanner.tim55x.com.ComNotRunningException;
+import org.apache.commons.cli.*;
+import org.apache.logging.log4j.core.util.ArrayUtils;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -33,13 +37,13 @@ public class LidarService {
         IScanOperator iScanOperator = new IScanOperator() {
             @Override
             public void newStateActice(State state) {
-                logger.info("New laser state active: " + state);
+                logger.info("{}: New laser state active: {}", instanceName, state);
                 gatewayClient.readyToPublish(gatewayClient.getContract().STATUS_STATE, new LidarState(state));
             }
 
             @Override
             public void errorOccured() {
-                logger.error("Laser ERROR");
+                logger.error("{}: Laser ERROR", instanceName);
             }
         };
 
@@ -50,7 +54,7 @@ public class LidarService {
             try {
 
                 for(LidarIntent intent : gatewayClient.toMessageSet(payload, LidarIntent.class)){
-                    logger.log(Level.INFO, "Received intent: " + intent);
+                    logger.log(Level.INFO, "{}: Received intent: {} ", instanceName, intent);
                     switch (intent.command){
                         case CONT_MEAS_START:
                             this.scanner.startContMeas();
@@ -89,21 +93,59 @@ public class LidarService {
     public static void main(String[] args) throws MqttException, InterruptedException, IOException {
         System.out.println("Loglevel= " + logger.getLevel());
         URI mqttURI = URI.create("tcp://127.0.0.1:1883");
-        if (args.length > 0) {
-            mqttURI = URI.create(args[0]);
+
+        Options options = new Options();
+        Option broker = new Option("b", "broker", true, "brocker to use, format: tcp://127.0.0.1:1883");
+        broker.setRequired(false);
+        options.addOption(broker);
+
+        Option sensors = new Option("s", "sensor", true, "sensor address");
+        sensors.setRequired(false);
+        sensors.setArgs(Option.UNLIMITED_VALUES);
+        options.addOption(sensors);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+
+            System.exit(1);
+            return;
+        }
+
+        String brokerArg = cmd.getOptionValue("broker");
+        String[] sensorArgs = cmd.getOptionValues("sensor");
+
+        if (brokerArg != null) {
+            mqttURI = URI.create(brokerArg);
         } else {
             logger.info("Per default, 'tcp://127.0.0.1:1883' is chosen. You can provide another address as first argument i.e.: tcp://iot.eclipse.org:1883");
         }
-        logger.info(mqttURI + " will be used as broker address.");
 
-        String lidarIp= "192.168.91.2";
+        ArrayList<String> sensorArgsList = new ArrayList<>();
+        if (sensorArgs != null) {
+            sensorArgsList.addAll(Arrays.asList(sensorArgs));
+        } else {
+            sensorArgsList.add("192.168.91.2");
+            logger.info("Per default, we expect our lidar sensor on IP '192.168.91.2.");
+        }
+
+        logger.info("Configured {} lidar sensors: {}", sensorArgsList.size(), sensorArgsList);
+        logger.info("{} will be used as broker address.", mqttURI);
+
         int lidarPort = 2112;
 
-        String mqttClientName = lidarIp + "@" + computerName;
-        String instanceName = mqttClientName;
+        for (String lidarIp : sensorArgsList) {
+            String mqttClientName = lidarIp + "@" + computerName;
+            String instanceName = mqttClientName;
 
-        LidarService lidarService = new LidarService(mqttURI, mqttClientName, instanceName, lidarIp, lidarPort);
-
+            LidarService lidarService = new LidarService(mqttURI, mqttClientName, instanceName, lidarIp, lidarPort);
+        }
         System.in.read();
     }
 }
